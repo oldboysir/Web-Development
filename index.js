10a880dcb723c3c742b340c95dd293c8c76db078
oldboysir <mshaheryarsiddiqui615@gmail.com>
2025-02-14 17:03:54 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+// let text = "57"
+// let textNum = Boolean(text)
+// console.log(typeof textNum);
+// console.log(textNum);
+// console.log(3>5);
+// console.log(3<5);
+// console.log(5=="5");
+// console.log(5==="5");
+// let x = 6
+// let y = 3.142
+// let z = (x/y)
+// console.log(z);
+let x = "Babar"
+if (x==="Babar"){
+    console.log('Babar is the goat');
+}
+else {
+    console.log('Overrated Youtuber is the goat');
+}
+
+
+
+
+
+
